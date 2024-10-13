3b5028d2b24acc022b0814cd615921cf8423e892
Sarthakaushal <sarthakkaushal@outlook.com>
2024-10-13 10:02:59 -0400

    Added data extraction from right camera into datakit and gitignore

--- data_scripts.py
+++ data_scripts.py
@@ -1,5 +1,8 @@
 import numpy as np
 import open3d as o3d
+import os
+import shutil
+from tqdm import tqdm
 
 def read_velodyne_bin(file_path):
     # Load the .bin file
@@ -24,10 +27,57 @@ def visualize_point_cloud(xyz):
     o3d.visualization.draw_geometries([pcd])
     o3d.io.write_point_cloud(output_file_path, pcd)
 
+def extract_sensor_data(root_data_path:str, new_sensor_data_path:str, 
+                     target_dir_name:str):
+    '''
+    Desc:   Basis on the current training and test data, fetches data from new 
+            sensor data dir
+    
+    Args:
+    root_data_dir : str : dir where the data already exists
+    new_sensor_data_path : str : dir where new sensor data exists
+    target_dir_name : str : name of new sensor as needed in root_data_dir
+    
+    Creator : Sarthakaushal
+    Date : 10/11/24
+    
+    Returns : None
+    '''
+    sub_folders = os.listdir(root_data_path+'/image_left')
+    sub_folders = [x  for x in sub_folders if '.' not in x ]
+    metadata = {}
+    for folder in sub_folders:
+        metadata[folder] = os.listdir(os.path.join(root_data_path, 'image_left', folder))
+    
+    target_data_path = os.path.join(root_data_path, 'image_right')    
+    if not target_dir_name in os.listdir(root_data_path):
+        os.mkdir(target_data_path)
+    
+    for folder in metadata.keys():
+        imgs = metadata[folder]
+        img_path = os.path.join(new_sensor_data_path, folder)
+        list_of_new_images = os.listdir(img_path)
+        
+        # Check if the image tag exists in the new sensor data
+        for img in imgs:
+            if not img in list_of_new_images:
+                raise FileNotFoundError
+        
+        # transfer data to new location
+        os.mkdir(os.path.join(target_data_path, folder))
+        for img in tqdm(imgs):
+            shutil.copy(os.path.join(root_data_path, 'image_left', folder, img),
+                        os.path.join(target_data_path,folder,img))
+
 if __name__ == "__main__":
     # Path to your .bin file
-    bin_file_path = "dev_datakit/velodyne/training/000044.bin"
+    bin_file_path = "data/dev_datakit/velodyne/training/000044.bin"
     
     # Read and visualize
     point_cloud_xyz = read_velodyne_bin(bin_file_path)
     visualize_point_cloud(point_cloud_xyz)
+    
+    root_data_path = 'data/dev_datakit'
+    new_sensor_data_path = 'data/data_object_image_3'
+    if 'image_right' not in os.listdir(root_data_path):
+        extract_sensor_data(root_data_path, new_sensor_data_path, 'image_right')
